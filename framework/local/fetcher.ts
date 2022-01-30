d8da7b5f19b09e43fe2f67f16de8d70ee5f473f1
hrr2000 <thedragonea@gmail.com>
2022-01-30 11:55:39 +0200

    testing cards api

--- framework/local/fetcher.ts
+++ framework/local/fetcher.ts
@@ -1,10 +1,25 @@
 import { Fetcher } from '@commerce/utils/types'
+import fetch from 'node-fetch'
+import { json } from 'stream/consumers'
 
 export const fetcher: Fetcher = async () => {
   console.log('FETCHER')
-  const res = await fetch('./data.json')
+  const res = await fetch('https://stagewrapper.ocstaging.net/bitaqatywrapper/new-cards', {
+    method: 'POST',
+    headers: {
+      contentType: 'application/json',
+      'Accept': 'application/json',
+    },
+    body: JSON.stringify({ 
+      channel: "WEB",
+      appName: "Bitaqaty",
+      locale: "en",
+      storeCurrencyId: 1,
+      bitaqatyStore: 1
+    })
+  })
   if (res.ok) {
-    const { data } = await res.json()
+    const data = await res.json()
     return data
   }
   throw res
